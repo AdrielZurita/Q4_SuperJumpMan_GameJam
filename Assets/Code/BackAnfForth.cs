d396896f96806ecfba1e81571b063c25cbc95fb0
RyanParson <149078913+RyanParson@users.noreply.github.com>
2026-05-26 10:57:16 -0400

    added stuff to code scene

--- Assets/Code/BackAnfForth.cs
+++ Assets/Code/BackAnfForth.cs
@@ -8,6 +8,7 @@ public class BackAnfForth : MonoBehaviour
     public int direction = 1; // 1 for right, -1 for left
     public float jankCompenstation = 0.5f;
     public LayerMask whatIsGround;
+    public LayerMask whatIsPlayer;
     public GameObject thisObject;
     private float speed;
 
@@ -24,7 +25,7 @@ public class BackAnfForth : MonoBehaviour
 
         transform.Translate(new Vector2(direction, 0) * speed * Time.deltaTime);
 
-        if (Physics2D.Raycast(thisObject.transform.position, new Vector2(direction, 0), 0.7f, whatIsGround))
+        if (Physics2D.Raycast(thisObject.transform.position, new Vector2(direction, 0), 0.7f, whatIsGround) || Physics2D.Raycast(thisObject.transform.position, new Vector2(direction, 0), 0.45f, whatIsPlayer))
         {
             direction *= -1; // Reverse direction
         }
